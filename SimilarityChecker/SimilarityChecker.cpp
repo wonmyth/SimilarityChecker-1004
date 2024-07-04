a36b63fc431a20eb75b848ce7a96f57c29bfcd59
cwon.lee <wonmyth@naver.com>
2024-07-04 09:46:00 +0900

    (feature) modify calLengthScore() for request changes

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -25,16 +25,23 @@ private:
 	bool isDoubleLength(int a, int b) {
 		return (a >= b * 2 || b >= a * 2);
 	}
+	int getLongLength(int a, int b) {
+		if (a > b) return a;
+		else return b;
+	}
+	int getShortLength(int a, int b) {
+		if (a > b) return b;
+		else return a;
+	}
 	int calLengthScore(int a, int b) {
-		int gap, ret;
-		if (a > b) {
-			gap = a - b;
-			ret = MAX_SCORE_STRING_LENGTH - MAX_SCORE_STRING_LENGTH * gap / b;
-		}
-		else {
-			gap = b - a;
-			ret = MAX_SCORE_STRING_LENGTH - MAX_SCORE_STRING_LENGTH * gap / a;
-		}
-		return ret;
+		double gap, ret;
+
+		int long_length  = getLongLength(a,b);
+		int short_length = getShortLength(a, b);
+
+		gap = long_length - short_length;
+		ret = (1 - gap / short_length) * MAX_SCORE_STRING_LENGTH;
+		
+		return (int)ret;
 	}
 };
